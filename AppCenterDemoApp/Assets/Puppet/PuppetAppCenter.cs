b614252514413e4ada0efff39dab3c2fc00b0b75
YuyaoHu <55774868+YuyaoHu@users.noreply.github.com>
2021-01-28 14:38:41 +0800

    GitHub-connect-test-Yuyao
    GitHub-connect-test-Yuyao

--- AppCenterDemoApp/Assets/Puppet/PuppetAppCenter.cs
+++ AppCenterDemoApp/Assets/Puppet/PuppetAppCenter.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 GitHub-connect-test-Yuyao
+GitHub-connect-test-Yuyao
 using Assets.AppCenter.Plugins.Android.Utility;
 using Microsoft.AppCenter.Unity;
 using Microsoft.AppCenter.Unity.Distribute;
